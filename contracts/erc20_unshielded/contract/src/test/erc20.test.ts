c629631deec6f870fdff91a1421a5f214d7db459
andrew <fleming-andrew@protonmail.com>
2025-03-23 21:01:00 -0500

    add tests for _approve and _burn

--- contracts/erc20_unshielded/contract/src/test/erc20.test.ts
+++ contracts/erc20_unshielded/contract/src/test/erc20.test.ts
@@ -67,8 +67,8 @@ describe('ERC20', () => {
   });
 
   describe('approve', () => {
+    // TODO: change caller context to test IERC20 interface
     it.skip('should approve', () => {
-      // TODO: need to init mapping
       const initAllowance = token.allowance(OWNER, SPENDER);
       expect(initAllowance).toEqual(0n);
 
@@ -76,6 +76,69 @@ describe('ERC20', () => {
       token.approve(SPENDER, AMOUNT);
     });
   })
+
+  describe('_approve', () => {
+    it('should _approve', () => {
+      const initAllowance = token.allowance(OWNER, SPENDER);
+      expect(initAllowance).toEqual(0n);
+
+      token._approve(OWNER, SPENDER, AMOUNT);
+
+      expect(token.allowance(OWNER, SPENDER)).toEqual(initAllowance + AMOUNT);
+    });
+
+    it('should throw when owner is zero', () => {
+      expect(() => {
+        token._approve(utils.ZERO_KEY, SPENDER, AMOUNT);
+      }).toThrow('ERC20: invalid owner');
+    });
+
+    it('should throw when spender is zero', () => {
+      expect(() => {
+        token._approve(OWNER, utils.ZERO_KEY, AMOUNT);
+      }).toThrow('ERC20: invalid spender');
+    });
+  });
+
+  describe('transferFrom', () => {
+    beforeEach(() => {
+      token._mint(OWNER, AMOUNT);
+    });
+
+    it.skip('should transfer from owner to recipient', () => {
+      const ownerBal = token.balanceOf(OWNER);
+      const recipientBal = token.balanceOf(RECIPIENT);
+
+      token.transferFrom(OWNER, RECIPIENT, AMOUNT);
+    })
+  })
+
+  describe('transfer', () => {
+    beforeEach(() => {
+      token._mint(OWNER, AMOUNT);
+    });
+
+    describe('internal _transfer', () => {
+      it('should _transfer', () => {
+        const ownerBal = token.balanceOf(OWNER);
+        const recipientBal = token.balanceOf(RECIPIENT);
+
+        token._transfer(OWNER, RECIPIENT, AMOUNT);
+
+        expect(token.balanceOf(OWNER)).toEqual(ownerBal - AMOUNT);
+        expect(token.balanceOf(RECIPIENT)).toEqual(recipientBal + AMOUNT);
+
+        // Confirm totalSupply doesn't change
+        expect(token.getLedger().totalSupply).toEqual(AMOUNT);
+      });
+
+      it('throws when _transfer with not enough balance', () => {
+        expect(() => {
+          token._transfer(OWNER, RECIPIENT, AMOUNT + 1n);
+        }).toThrow('ERC20: insufficient balance');
+      });
+    });
+  });
 
   describe('mint', () => {
     it('should mint and update supply', () => {
@@ -97,6 +160,32 @@ describe('ERC20', () => {
       expect(() => {
         token._mint(utils.ZERO_ADDRESS, AMOUNT);
       }).toThrow('ERC20: invalid receiver');
+    });
+  });
+
+  describe('burn', () => {
+    beforeEach(() => {
+      token._mint(OWNER, AMOUNT);
+    });
+
+    it('should burn tokens', () => {
+      token._burn(OWNER, 1n);
+
+      const afterBurn = AMOUNT - 1n;
+      expect(token.balanceOf(OWNER)).toEqual(afterBurn);
+      expect(token.getLedger().totalSupply).toEqual(afterBurn)
+    });
+
+    it('should throw when burning from zero', () => {
+      expect(() => {
+        token._burn(utils.ZERO_KEY, AMOUNT);
+      }).toThrow('ERC20: invalid sender');
+    });
+
+    it('should throw when burn amount is greater than balance', () => {
+      expect(() => {
+        token._burn(OWNER, AMOUNT + 1n);
+      }).toThrow('ERC20: insufficient balance');
     });
   });
 });

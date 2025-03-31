f33a5e66ae30eb2e3563deaad612f0e4beee4cc4
andrew <fleming-andrew@protonmail.com>
2025-03-31 16:51:00 -0500

    remove token dir

--- contracts/erc20/contract/src/witnesses.ts
+++
@@ -1,7 +0,0 @@
-// This is how we type an empty object.
-export type ERC20PrivateState = Record<string, never>;
-
-export const witnesses = {};
-
-export const createERC20PrivateState = () => ({
-});

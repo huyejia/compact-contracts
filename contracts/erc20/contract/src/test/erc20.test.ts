d386263e145875735bf5ddbf7d32b85e23add5ea
andrew <fleming-andrew@protonmail.com>
2025-04-02 23:02:52 -0500

    add more tests

--- contracts/erc20/contract/src/test/erc20.test.ts
+++ contracts/erc20/contract/src/test/erc20.test.ts
@@ -1,10 +1,5 @@
-import {
-    type CoinPublicKey,
-    decodeCoinPublicKey,
-  } from '@midnight-ntwrk/compact-runtime';
-  import { sampleCoinPublicKey } from '@midnight-ntwrk/zswap';
-  import * as MockERC20Contract from '../artifacts/MockERC20/contract/index.cjs';
-  import { ERC20ContractSimulator } from './simulator/ERC20Simulator';
+import { CoinPublicKey } from '@midnight-ntwrk/compact-runtime';
+import { ERC20ContractSimulator } from './ERC20Simulator';
 import { MaybeString } from './types.js';
 import * as utils from './utils.js';
 
@@ -23,52 +18,265 @@ const SYMBOL: MaybeString = {
 const DECIMALS: bigint = 18n;
 
 const AMOUNT: bigint = BigInt(250);
-const MAX_UINT64 = BigInt(2**64) - BigInt(1);
 const MAX_UINT128 = BigInt(2**128) - BigInt(1);
 
-const OWNER = utils.createEitherTestUser('OWNER');
-const RECIPIENT = utils.createEitherTestUser('RECIPIENT');
-const SPENDER = utils.createEitherTestUser('SPENDER');
+const OWNER = String(Buffer.from("OWNER", 'ascii').toString('hex')).padStart(64, '0');
+const SPENDER = String(Buffer.from("SPENDER", 'ascii').toString('hex')).padStart(64, '0');
+const UNAUTHORIZED = String(Buffer.from("UNAUTHORIZED", 'ascii').toString('hex')).padStart(64, '0');
+const ZERO = String().padStart(64, '0');
+const Z_OWNER = utils.createEitherTestUser('OWNER');
+const Z_RECIPIENT = utils.createEitherTestUser('RECIPIENT');
+const Z_SPENDER = utils.createEitherTestUser('SPENDER');
 const SOME_CONTRACT = utils.createEitherTestContractAddress('SOME_CONTRACT');
 
-
 let token: ERC20ContractSimulator;
+let caller: CoinPublicKey;
 
 describe('ERC20', () => {
+  describe('initializer', () => {
+    it('should initialize metadata', () => {
+      token = new ERC20ContractSimulator(NAME, SYMBOL, DECIMALS);
+
+      expect(token.getCurrentPublicState().eRC20Name).toEqual(NAME);
+      expect(token.getCurrentPublicState().eRC20Symbol).toEqual(SYMBOL);
+      expect(token.getCurrentPublicState().eRC20Decimals).toEqual(DECIMALS);
+    });
+
+    it('should initialize empty metadata', () => {
+      const NO_DECIMALS = 0n;
+      token = new ERC20ContractSimulator(NO_STRING, NO_STRING, NO_DECIMALS);
+
+      expect(token.getCurrentPublicState().eRC20Name).toEqual(NO_STRING);
+      expect(token.getCurrentPublicState().eRC20Symbol).toEqual(NO_STRING);
+      expect(token.getCurrentPublicState().eRC20Decimals).toEqual(NO_DECIMALS);
+    });
+  });
+
   beforeEach(() => {
     token = new ERC20ContractSimulator(NAME, SYMBOL, DECIMALS);
   });
 
-  describe('approve', () => {
-    // TODO: change caller context to test IERC20 interface
-    it.skip('should approve', () => {
-      const initAllowance = token.allowance(OWNER, SPENDER);
-      expect(initAllowance).toEqual(0n);
-
-      token._mint(OWNER, AMOUNT);
-      token.approve(SPENDER, AMOUNT);
+  describe('balanceOf', () => {
+    it('returns zero when requested account has no balance', () => {
+      expect(token.balanceOf(Z_OWNER)).toEqual(0n);
     });
+
+    it('returns balance when requested account has tokens', () => {
+      token._mint(Z_OWNER, AMOUNT);
+      expect(token.balanceOf(Z_OWNER)).toEqual(AMOUNT);
+    });
+  });
+
+  describe('transfer', () => {
+    beforeEach(() => {
+      token._mint(Z_OWNER, AMOUNT);
+
+      expect(token.balanceOf(Z_OWNER)).toEqual(AMOUNT);
+      expect(token.balanceOf(Z_RECIPIENT)).toEqual(0n);
+    });
+
+    afterEach(() => {
+      expect(token.getCurrentPublicState().eRC20TotalSupply).toEqual(AMOUNT);
+    });
+
+    it('should transfer partial', () => {
+      const partialAmt = AMOUNT - 1n;
+      caller = OWNER;
+      const txSuccess = token.transfer(Z_RECIPIENT, partialAmt, caller);
+
+      expect(txSuccess).toBeTruthy;
+      expect(token.balanceOf(Z_OWNER)).toEqual(1n);
+      expect(token.balanceOf(Z_RECIPIENT)).toEqual(partialAmt);
+    });
+
+    it('should transfer full', () => {
+      caller = OWNER;
+      const txSuccess = token.transfer(Z_RECIPIENT, AMOUNT, caller);
+
+      expect(txSuccess).toBeTruthy;
+      expect(token.balanceOf(Z_OWNER)).toEqual(0n);
+      expect(token.balanceOf(Z_RECIPIENT)).toEqual(AMOUNT);
+    });
+
+    it('should fail with insufficient balance', () => {
+      caller = OWNER;
+
+      expect(() => {
+        token.transfer(Z_RECIPIENT, AMOUNT + 1n, caller);
+      }).toThrow('ERC20: insufficient balance');
+    });
+
+    it('should fail with transfer from zero', () => {
+      caller = ZERO;
+
+      expect(() => {
+        token.transfer(Z_RECIPIENT, AMOUNT, caller);
+      }).toThrow('ERC20: invalid sender');
+    });
+
+    it('should fail with transfer to zero', () => {
+      caller = OWNER;
+
+      expect(() => {
+        token.transfer(utils.ZERO_ADDRESS, AMOUNT, caller);
+      }).toThrow('ERC20: invalid receiver');
+    });
+  });
+
+  describe('approve', () => {
+    beforeEach(() => {
+      expect(token.allowance(Z_OWNER, Z_SPENDER)).toEqual(0n);
+    });
+
+    it('should approve and update allowance', () => {
+      caller = OWNER;
+
+      token.approve(Z_SPENDER, AMOUNT, caller);
+      expect(token.allowance(Z_OWNER, Z_SPENDER)).toEqual(AMOUNT);
+    });
+
+    it('should fail when approve from zero', () => {
+      caller = ZERO;
+
+      expect(() => {
+        token.approve(Z_SPENDER, AMOUNT, caller);
+      }).toThrow('ERC20: invalid owner');
+    });
+
+    it('should fail when approve to zero', () => {
+      caller = OWNER;
+
+      expect(() => {
+        token.approve(utils.ZERO_ADDRESS, AMOUNT, caller);
+      }).toThrow('ERC20: invalid spender');
+    });
+  });
+
+  describe('transferFrom', () => {
+    beforeEach(() => {
+      caller = OWNER;
+
+      token.approve(Z_SPENDER, AMOUNT, caller);
+      token._mint(Z_OWNER, AMOUNT);
+    });
+
+    afterEach(() => {
+      expect(token.getCurrentPublicState().eRC20TotalSupply).toEqual(AMOUNT);
+    });
+
+    it('should transferFrom spender (partial)', () => {
+      caller = SPENDER;
+      const partialAmt = AMOUNT - 1n;
+
+      const txSuccess = token.transferFrom(Z_OWNER, Z_RECIPIENT, partialAmt, caller);
+      expect(txSuccess).toBeTruthy;
+
+      // Check balances
+      expect(token.balanceOf(Z_OWNER)).toEqual(1n);
+      expect(token.balanceOf(Z_RECIPIENT)).toEqual(partialAmt);
+      // Check leftover allowance
+      expect(token.allowance(Z_OWNER, Z_SPENDER)).toEqual(1n);
+    });
+
+    it('should transferFrom spender (full)', () => {
+      caller = SPENDER;
+
+      const txSuccess = token.transferFrom(Z_OWNER, Z_RECIPIENT, AMOUNT, caller);
+      expect(txSuccess).toBeTruthy;
+
+      // Check balances
+      expect(token.balanceOf(Z_OWNER)).toEqual(0n);
+      expect(token.balanceOf(Z_RECIPIENT)).toEqual(AMOUNT);
+      // Check no allowance
+      expect(token.allowance(Z_OWNER, Z_SPENDER)).toEqual(0n);
+    });
+
+    it('should transferFrom and not consume infinite allowance', () => {
+      caller = OWNER;
+      token.approve(Z_SPENDER, MAX_UINT128, caller);
+
+      caller = SPENDER;
+      const txSuccess = token.transferFrom(Z_OWNER, Z_RECIPIENT, AMOUNT, caller);
+      expect(txSuccess).toBeTruthy;
+
+      // Check balances
+      expect(token.balanceOf(Z_OWNER)).toEqual(0n);
+      expect(token.balanceOf(Z_RECIPIENT)).toEqual(AMOUNT);
+      // Check infinite allowance
+      expect(token.allowance(Z_OWNER, Z_SPENDER)).toEqual(MAX_UINT128);
+    });
+
+    it ('should fail when transfer amount exceeds allowance', () => {
+      caller = SPENDER;
+
+      expect(() => {
+        token.transferFrom(Z_OWNER, Z_RECIPIENT, AMOUNT + 1n);
+      }).toThrow('ERC20: insufficient allowance');
+    });
+
+    it ('should fail when transfer amount exceeds balance', () => {
+      caller = OWNER;
+      // Increase allowance > balance
+      token.approve(Z_SPENDER, AMOUNT + 1n, caller);
+
+      caller = SPENDER;
+      expect(() => {
+        token.transferFrom(Z_OWNER, Z_RECIPIENT, AMOUNT + 1n, caller);
+      }).toThrow('ERC20: insufficient balance');
+    });
+
+    it('should fail when spender does not have allowance', () => {
+      caller = UNAUTHORIZED;
+
+      expect(() => {
+        token.transferFrom(Z_OWNER, Z_RECIPIENT, AMOUNT, caller);
+      }).toThrow("ERC20: insufficient allowance");
+    });
+
+    it('should fail to transferFrom zero address', () => {
+      caller = ZERO;
+
+      expect(() => {
+        token.transferFrom(Z_OWNER, Z_RECIPIENT, AMOUNT, caller);
+      }).toThrow("ERC20: insufficient allowance");
+    });
+
+    it('should fail to transferFrom to the zero address', () => {
+      caller = SPENDER;
+
+      expect(() => {
+        token.transferFrom(Z_OWNER, utils.ZERO_ADDRESS, AMOUNT, caller);
+      }).toThrow("ERC20: invalid receiver");
+    });
+  });
+
+  describe('_transfer', () => {
+    beforeEach(() => {
+      token._mint(Z_OWNER, AMOUNT);
+    });
+
+    afterEach(() => {
+      expect(token.getCurrentPublicState().eRC20TotalSupply).toEqual(AMOUNT);
+    });
+
+    it('should update balances (partial)', () => {
+      const partialAmt = AMOUNT - 1n;
+      token._transfer(Z_OWNER, Z_RECIPIENT, partialAmt);
+
+      expect(token.balanceOf(Z_OWNER)).toEqual(1n);
+      expect(token.balanceOf(Z_RECIPIENT)).toEqual(partialAmt);
+    })
   })
 
-  describe('mint', () => {
+  describe('_mint', () => {
     it('should mint and update supply', () => {
       const initSupply = token.getCurrentPublicState().eRC20TotalSupply;
       expect(initSupply).toEqual(0n);
 
-      let newState = token._mint(RECIPIENT, AMOUNT);
-      expect(newState.eRC20TotalSupply).toEqual(AMOUNT);
-      expect(token.balanceOf(RECIPIENT)).toEqual(AMOUNT);
+      token._mint(Z_RECIPIENT, AMOUNT);
       expect(token.getCurrentPublicState().eRC20TotalSupply).toEqual(AMOUNT);
-
-      newState = token._transfer(RECIPIENT, OWNER, AMOUNT);
-      expect(token.balanceOf(OWNER)).toEqual(AMOUNT);
-      expect(token.balanceOf(RECIPIENT)).toEqual(0n);
-
-      const toHex = Buffer.from("OWNER", 'ascii').toString('hex');
-      const addy = String(toHex).padStart(64, '0');
-      token.transfer(RECIPIENT, AMOUNT, addy);
-
-      expect(token.balanceOf(OWNER)).toEqual(0n);
+      expect(token.balanceOf(Z_RECIPIENT)).toEqual(AMOUNT);
+      expect(token.getCurrentPublicState().eRC20TotalSupply).toEqual(AMOUNT);
     });
 
     it('should not mint to zero pubkey', () => {
@@ -84,65 +292,65 @@ describe('ERC20', () => {
     });
   });
 
-//  describe('burn', () => {
-//    beforeEach(() => {
-//      token._mint(OWNER, AMOUNT);
-//    });
-//
-//    it('should burn tokens', () => {
-//      token._burn(OWNER, 1n);
-//
-//      const afterBurn = AMOUNT - 1n;
-//      expect(token.balanceOf(OWNER)).toEqual(afterBurn);
-//      expect(token.getCurrentPublicState().eRC20TotalSupply).toEqual(afterBurn)
-//    });
-//
-//    it('should throw when burning from zero', () => {
-//      expect(() => {
-//        token._burn(utils.ZERO_KEY, AMOUNT);
-//      }).toThrow('ERC20: invalid sender');
-//    });
-//
-//    it('should throw when burn amount is greater than balance', () => {
-//      expect(() => {
-//        token._burn(OWNER, AMOUNT + 1n);
-//      }).toThrow('ERC20: insufficient balance');
-//    });
-//  });
-//
-//  describe('_update', () => {
-//    it('should update from zero to non-zero (mint)', () => {
-//      expect(token.getCurrentPublicState().eRC20TotalSupply).toEqual(0n);
-//      expect(token.balanceOf(OWNER)).toEqual(0n);
-//
-//      token._update(utils.ZERO_KEY, OWNER, AMOUNT);
-//
-//      expect(token.getCurrentPublicState().eRC20TotalSupply).toEqual(AMOUNT);
-//      expect(token.balanceOf(OWNER)).toEqual(AMOUNT);
-//    });
-//
-//    describe('with minted tokens', () => {
-//      beforeEach(() => {
-//        token._update(utils.ZERO_ADDRESS, OWNER, AMOUNT);
-//
-//        expect(token.getCurrentPublicState().eRC20TotalSupply).toEqual(AMOUNT);
-//        expect(token.balanceOf(OWNER)).toEqual(AMOUNT);
-//      });
-//
-//      it('should update from non-zero to zero (burn)', () => {
-//        token._update(OWNER, utils.ZERO_ADDRESS, AMOUNT);
-//
-//        expect(token.getCurrentPublicState().eRC20TotalSupply).toEqual(0n);
-//        expect(token.balanceOf(OWNER)).toEqual(0n);
-//      });
-//
-//      it('should update from non-zero to non-zero (transfer)', () => {
-//        token._update(OWNER, RECIPIENT, AMOUNT - 1n);
-//
-//        expect(token.getCurrentPublicState().eRC20TotalSupply).toEqual(AMOUNT);
-//        expect(token.balanceOf(OWNER)).toEqual(1n);
-//        expect(token.balanceOf(RECIPIENT)).toEqual(AMOUNT - 1n);
-//      });
-//    });
-//  });
+  describe('_burn', () => {
+    beforeEach(() => {
+      token._mint(Z_OWNER, AMOUNT);
+    });
+
+    it('should burn tokens', () => {
+      token._burn(Z_OWNER, 1n);
+
+      const afterBurn = AMOUNT - 1n;
+      expect(token.balanceOf(Z_OWNER)).toEqual(afterBurn);
+      expect(token.getCurrentPublicState().eRC20TotalSupply).toEqual(afterBurn)
+    });
+
+    it('should throw when burning from zero', () => {
+      expect(() => {
+        token._burn(utils.ZERO_KEY, AMOUNT);
+      }).toThrow('ERC20: invalid sender');
+    });
+
+    it('should throw when burn amount is greater than balance', () => {
+      expect(() => {
+        token._burn(Z_OWNER, AMOUNT + 1n);
+      }).toThrow('ERC20: insufficient balance');
+    });
+  });
+
+  describe('_update', () => {
+    it('should update from zero to non-zero (mint)', () => {
+      expect(token.getCurrentPublicState().eRC20TotalSupply).toEqual(0n);
+      expect(token.balanceOf(Z_OWNER)).toEqual(0n);
+
+      token._update(utils.ZERO_KEY, Z_OWNER, AMOUNT);
+
+      expect(token.getCurrentPublicState().eRC20TotalSupply).toEqual(AMOUNT);
+      expect(token.balanceOf(Z_OWNER)).toEqual(AMOUNT);
+    });
+
+    describe('with minted tokens', () => {
+      beforeEach(() => {
+        token._update(utils.ZERO_ADDRESS, Z_OWNER, AMOUNT);
+
+        expect(token.getCurrentPublicState().eRC20TotalSupply).toEqual(AMOUNT);
+        expect(token.balanceOf(Z_OWNER)).toEqual(AMOUNT);
+      });
+
+      it('should update from non-zero to zero (burn)', () => {
+        token._update(Z_OWNER, utils.ZERO_ADDRESS, AMOUNT);
+
+        expect(token.getCurrentPublicState().eRC20TotalSupply).toEqual(0n);
+        expect(token.balanceOf(Z_OWNER)).toEqual(0n);
+      });
+
+      it('should update from non-zero to non-zero (transfer)', () => {
+        token._update(Z_OWNER, Z_RECIPIENT, AMOUNT - 1n);
+
+        expect(token.getCurrentPublicState().eRC20TotalSupply).toEqual(AMOUNT);
+        expect(token.balanceOf(Z_OWNER)).toEqual(1n);
+        expect(token.balanceOf(Z_RECIPIENT)).toEqual(AMOUNT - 1n);
+      });
+    });
+  });
 });
